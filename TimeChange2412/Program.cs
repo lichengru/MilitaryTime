dae7b0013c2868b19624cf73c1d309d4a4bb1f63
gjo4us06c961115@gmail.com <gjo4us06c961115@gmail.com>
2020-01-08 21:13:59 +0800

    加入專案檔案。

---
+++ TimeChange2412/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace TimeChange2412
+{
+    class Program
+    {
+        public static void Main()
+        {
+            Regexcheck regex = new Regexcheck();
+            Console.WriteLine("請輸入24小時制的時間(格式為00:00): ");
+            regex.Change();           
+            Console.ReadKey();
+            
+        }
+            
+    }
+}
---
+++ TimeChange2412/Regexcheck.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace TimeChange2412
+{
+    public class Regexcheck
+    {
+        
+        string pattern = @"^(20|21|22|23|[0-1]\d)\:[0-5]\d$";
+        string input;
+        int Iu;
+        string[] clock;
+        
+        public void Timeset()
+        {
+
+            clock = input.Split(':');
+            Iu = Convert.ToInt32(clock[0]);
+            switch (Iu)
+            {
+                case 12:
+                    Console.WriteLine(Iu+":" + clock[1] + " PM");
+                    break;
+                case 13:                   
+                case 14:                   
+                case 15:                  
+                case 16:               
+                case 17:            
+                case 18:                 
+                case 19:                 
+                case 20:                  
+                case 21:                  
+                case 22:                
+                case 23:
+                    Console.WriteLine(Iu-12+":" + clock[1] + " PM");
+                    break;
+                case 00:
+                    Console.WriteLine("00:" + clock[1] + " AM");
+                    break;
+
+                default:
+                    Console.WriteLine(Iu + ":" + clock[1] + " AM");
+                    break;
+            }
+          
+        }
+        public void Change()
+        {
+            input = Console.ReadLine();
+            Match match = Regex.Match(input, pattern);
+            if (match.Success)
+            {
+                Timeset();
+            }
+            else
+            {
+                Console.WriteLine("格式輸入錯誤，請重新輸入");
+                Change();
+            }
+        }
+    }
+}
